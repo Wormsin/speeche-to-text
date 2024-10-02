32e7159d4dd35b35a4079592aea8a8319514a5dc
Wormsin <bobdomeme@gmail.com>
2024-10-02 17:49:06 +0300

    add new button

--- BP_CHROME_EXTENTION/chrome_ex/popup.js
+++ BP_CHROME_EXTENTION/chrome_ex/popup.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Находим кнопку
     let saveLogsButton = document.getElementById('save-logs');
-
+    let runPythonButton = document.getElementById('run-python');
+    
     // Добавляем обработчик клика
     saveLogsButton.addEventListener('click', function () {
         // Отправляем сообщение в content.js для сохранения логов
@@ -9,4 +10,23 @@ document.addEventListener('DOMContentLoaded', function () {
             chrome.tabs.sendMessage(tabs[0].id, { action: 'saveLogs' });
         });
     });
+
+
+    runPythonButton.addEventListener('click', function () {
+        fetch('http://localhost:8000', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ message: 'Запуск Python-кода из расширения' })
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Ответ от сервера:', data);
+            })
+            .catch(error => {
+                console.error('Ошибка:', error);
+            });
+    });
+
 });
